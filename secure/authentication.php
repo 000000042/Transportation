3839b83822648614354a5563729ef476243d5589
Hussein Banaei <hossein711@gmail.com>
2022-02-12 14:38:51 +0330

    12/2/2022

---
+++ secure/authentication.php
@@ -0,0 +1,28 @@
+    <?php      
+        include('connection.php');  
+        $username = $_POST['user'];  
+        $password = $_POST['pass'];  
+		
+   
+            //to prevent from mysqli injection  
+            $username = stripcslashes($username);  
+            $password = stripcslashes($password);  
+            $username = mysqli_real_escape_string($con, $username);  
+            $password = mysqli_real_escape_string($con, $password);  
+          
+            $sql = "select *from login where username = '$username' and password = '$password'";  
+            $result = mysqli_query($con, $sql);  
+            $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+            $count = mysqli_num_rows($result);  
+              
+            if($count == 1){  
+                echo "<h1><center> ورود شما در شرکت حمل و نقل سجاد موفقیت آمیز بود </center></h1>";
+				echo "<h1><center><input type='submit' name='btn' value='انتقال به صفحه جدید'></h1></center>";
+				
+				header('Location: http://google.com/');
+				exit();
+            }  
+            else{  
+                echo "<h1> تلاش شما برای اتصال به بخش خصوصی شرکت موفقت آمیز نبود ، دوباره تلاش کنید. </h1>";  
+            }     
+    ?>  
